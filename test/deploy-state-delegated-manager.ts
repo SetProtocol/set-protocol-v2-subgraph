96139e0e431f8ef7b4375fdbab85994ad31b8b8f
pblivin0x <pblivin0x@gmail.com>
2022-03-18 15:53:20 -0400

    add events for delegated manager role changes

--- test/deploy-state-delegated-manager.ts
+++ test/deploy-state-delegated-manager.ts
@@ -1,25 +1,95 @@
 // Deploy Hardhat Network State for Subgraph Tests
 // -----------------------------------------------
 // Deploy a test environment to hardhat for subgraph development
-// - Deploy system
+// - Setup
+//   - Deploy system
+//   - Deploy SetToken
+//   - Deploy ManagerCore and mock Extension
+//   - Deploy DelegatedManager
+//   - Deploy DelegatedManager
+//   - Add DelegatedManager to ManagerCore through factory
+// - Trigger events for testing
+//   - Update owner
+//   - Update methodologist
+//   - Add operatorTwo
+//   - Remove operatorOne
 
 import "module-alias/register";
-import { getAccounts, getSystemFixture } from "@setprotocol/set-protocol-v2/utils/test/index";
+import { ADDRESS_ZERO } from "@utils/constants";
+import { getSystemFixture } from "@setprotocol/set-protocol-v2/utils/test/index";
 import DeployHelper from "@utils/deploys";
+import {
+  ether,
+  getAccounts,
+} from "@utils/index";
 
 
 async function main() {
 
   console.log("Starting deployment");
 
-  const [owner] = await getAccounts();
+  const [
+    ownerOne,
+    ownerTwo,
+    methodologistOne,
+    methodologistTwo,
+    operatorOne,
+    operatorTwo,
+    factory
+  ] = await getAccounts();
+
+  // Setup
+  // -----------------------------------------------
 
   // Deploy system
-  const deployer = new DeployHelper(owner.wallet);
-  const setup = getSystemFixture(owner.address);
-  await setup.initialize();
+  const deployer = new DeployHelper(ownerOne.wallet);
+  const setV2Setup = getSystemFixture(ownerOne.address);
+  await setV2Setup.initialize();
 
+  // Deploy SetToken
+  const setToken = await setV2Setup.createSetToken(
+    [setV2Setup.dai.address],
+    [ether(1)],
+    [setV2Setup.issuanceModule.address]
+  );
+  await setV2Setup.issuanceModule.initialize(setToken.address, ADDRESS_ZERO);
+
+  // Deploy ManagerCore and mock Extension
   const managerCore = await deployer.managerCore.deployManagerCore();
+  const baseExtension = await deployer.mocks.deployBaseGlobalExtensionMock(managerCore.address);
+
+  // Deploy DelegatedManager
+  const delegatedManager = await deployer.manager.deployDelegatedManager(
+    setToken.address,
+    ownerOne.address,
+    methodologistOne.address,
+    [baseExtension.address],
+    [operatorOne.address],
+    [setV2Setup.usdc.address, setV2Setup.weth.address],
+    true
+  );
+
+  // Transfer ownership to DelegatedManager
+  await setToken.setManager(delegatedManager.address);
+
+  // Add DelegatedManager to ManagerCore through factory
+  await managerCore.initialize([factory.address]);
+  await managerCore.connect(factory.wallet).addManager(delegatedManager.address);
+
+  // Trigger events for testing
+  // -----------------------------------------------
+
+  // Update owner
+  await delegatedManager.connect(ownerOne.wallet).transferOwnership(ownerTwo.address);
+
+  // Update methodologist
+  await delegatedManager.connect(methodologistOne.wallet).setMethodologist(methodologistTwo.address);
+
+  // Add operatorTwo
+  await delegatedManager.connect(ownerTwo.wallet).addOperators([operatorTwo.address]);
+
+  // Remove operatorOne
+  await delegatedManager.connect(ownerTwo.wallet).removeOperators([operatorOne.address]);
 
 }
 
