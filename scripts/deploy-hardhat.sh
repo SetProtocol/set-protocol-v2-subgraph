d40a10c0809b316f9be3c3595b5f0b485532f1a3
Colin McRae <colin@setprotocol.com>
2022-03-23 09:32:57 -0700

    Add helper task to simplify local re-deploys and copy set-protocol-v2 artifacts into project to populate hardhat stack trace with contract signatures

--- scripts/deploy-hardhat.sh
+++ scripts/deploy-hardhat.sh
@@ -15,6 +15,9 @@ fi
 
 yarn chain --hostname 0.0.0.0 &
 
+# Copy set-protocol-v2 artifacts to populate contract signatures in Hardhat node stack trace
+cp -r node_modules/@setprotocol/set-protocol-v2/artifacts/* artifacts/
+
 # Wait for network deployment
 bash /app/scripts/wait-for-it.sh "localhost:${HARDHAT_PORT}" -t 20
 
